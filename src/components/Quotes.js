41a67aa8fe9512d69468bcf0f7fc38d0f510f508
meron93 <meronyemane18@gmail.com>
2017-09-02 12:43:24 -0700

    Search results static page implemented

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavBar from './NavBar';
 import SearchArea from './SearchArea';
+import SearchResults from './SearchResults';
 import './Quotes.css';
 
 export default class Quotes extends React.Component {
@@ -10,6 +11,7 @@ export default class Quotes extends React.Component {
         <NavBar />
         <h1>Search Quotes</h1>
         <SearchArea themes={["Relationships", "Finances", "Me", "Dating", "Fear", "Career", "Perseverance"]} />
+        <SearchResults />
       </div>
     );
   }
