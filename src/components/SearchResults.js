aba9e02a9700c0fbe6559893b933353739dda03d
meron93 <meronyemane18@gmail.com>
2017-10-07 20:18:06 -0700

    Author search results displaying

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -5,20 +5,25 @@ import IndividualQuotes from './IndividualQuotes';
 
 export class SearchResults extends React.Component {
   render() {
-    console.log("HELLLLOOOO")
-    console.log(this.props)
-    const quotesToDisplay = this.props.quotesToDisplay.map((quote, index) =>
-      <section key={index} className="quote-expanded">
+    console.log(this.props.quotesToDisplay)
+    if (this.props.quotesToDisplay.length > 0) {
+      const quotesToDisplay = this.props.quotesToDisplay[0].map((quote, index) =>
+        <section key={index} className="quote-expanded">
           <h2 className="quoteText"><span>"</span>{quote.quoteString}<span>"</span></h2>
           <h4 className="searchResultsAuthor"><span>- </span>{quote.author}</h4>
           <div>
             <h3>Theme(s): {quote.theme}</h3>
           </div>
-      </section>
-    );
+        </section>
+      );
+      return (
+        <div>
+          {quotesToDisplay} 
+        </div>
+      );
+    }
     return (
      <div> 
-      {quotesToDisplay}
      </div>
     );
   }
