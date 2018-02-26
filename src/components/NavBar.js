3b471cfea27b22b5d138e45d5b8b73b061a2649d
meron93 <meronyemane18@gmail.com>
2018-02-25 18:26:06 -0800

    Added setupTests folder/enzyme added to package.json

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -18,36 +18,6 @@ export class NavBar extends React.Component {
     this.props.dispatch(addQuoteDisplay([]));
     clearAuthToken(); 
   }
-
-  // <div className="navLinksToBeCentered">  
-  //           <div className="navigationLinks navigationLogo"><Link to='/'>QuoteCatcher</Link></div>
-  //           <div className="navigationLinks navHomeLink"><Link to='/'><span className="fa fa-home fa-2x navHomeLink"></span></Link></div>
-  //           <div className="navigationLinks"><Link to='/addquote'>Add Quote</Link></div>
-  //           <div className="navigationLinks"><Link to='/quotes'><span className="fa fa-search"></span>&nbsp;Search</Link></div>
-  //           <div className="navigationLinks navigationLinkLogout">{logOutButton}</div>
-  // </div>
-      //   <nav className="navbar navbar-inverse navbar-static-top" role="navigation">
-      //   <div className="container-fluid">
-      //     <div className="navbar-header">
-      //           <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-      //             <span className="sr-only">Toggle navigation</span>
-      //             <span className="icon-bar"></span>
-      //             <span className="icon-bar"></span>
-      //             <span className="icon-bar"></span>
-      //           </button>
-      //       </div>
-      //     <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
-      //       <ul class="nav navbar-nav">
-      //         <div className="navigationLinks navigationLogo"><Link to='/'>QuoteCatcher</Link></div>
-      //         <div className="navigationLinks"><Link to='/addquote'>Add Quote</Link></div>
-      //         <div className="navigationLinks"><Link to='/quotes'><span className="fa fa-search"></span>&nbsp;Search</Link></div>
-      //       </div>
-      //       <div class="nav navbar-nav navbar-right">
-      //         <div className="navigationLinks navigationLinkLogout">{logOutButton}</div>
-      //       </div>
-      //     </div>
-      //   </div>
-      // </nav>
   render () {
     let logOutButton;
     if (this.props.loggedIn) {
