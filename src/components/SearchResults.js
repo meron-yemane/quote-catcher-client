da8e198cd5dda4b72ceac01a6c631fa4a6c087cc
meron93 <meronyemane18@gmail.com>
2018-03-06 15:41:14 -0800

    Deleted comments and unused code from SearchResults component

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -26,9 +26,6 @@ export class SearchResults extends React.Component {
   onSubmit(value, quote) {
     return this.props
     .dispatch(addTheme(value.themesToAdd, quote._id))
-    // .then(() => {
-    //   return this.props.dispatch(reset('searchResults'))
-    // })
   }
 
   render() {
@@ -53,81 +50,6 @@ export class SearchResults extends React.Component {
       themesToDisplay = [];
       addQuoteThemes = [];
       themeOptions = [];
-      // const renderMultiselect = ({input, data}) => {
-      //   // if (this.props.quotesToDisplayAddThemeId === quote._id) {
-      //   //   previousThemeId = this.props.quotesToDisplayAddThemeId
-      //   // }
-      //   // console.log("quotes themes", this.props.quotesToDisplayAddThemeId)
-      //   // console.log("previousThemeId", previousThemeId)
-      //   // console.log(this.props.quotesToDisplayAddThemeId !== previousThemeId)
-      //   // console.log(this.props.quotesToDisplayAddThemeId !== null)
-      //   // console.log(previousThemeId !== undefined)
-
-      //   if (this.props.quotesToDisplayAddThemeId === quote._id) {
-      //     console.log(quote._id)
-      //     console.log("previous themes id", this.props.previousAddThemeQuotes[this.props.previousAddThemeQuotes.length -2])
-      //     if (this.props.quotesToDisplayAddThemeId !== this.props.previousAddThemeQuotes[this.props.previousAddThemeQuotes.length -3] && this.props.previousAddThemeQuotes[this.props.previousAddThemeQuotes.length -2] !== undefined && this.props.quotesToDisplayAddThemeId !== this.props.previousAddThemeQuotes[this.props.previousAddThemeQuotes.length -2]) {
-      //       console.log("inside different quote if/reset")
-      //       input.value = []
-      //       inputValue = input.value
-      //     }
-      //     // else if (this.props.)
-
-      //     else {
-      //       console.log("Inside else statement", input.value)
-      //       inputValue = input.value
-      //     }
-      //   } else {
-      //     console.log("not clicked quote")
-      //     inputValue = []
-      //   }
-
-      //   // if (this.props.previousAddThemeQuotes[-2] !== ) {
-      //   //   console.log("INSIDEEE")
-      //   //   input.value = []
-      //   //   inputValue = input.value
-      //   // }
-      //   // else if (this.props.quotesToDisplayAddThemeId === quote._id) {
-      //   //   inputValue = input.value
-      //   //   // previousThemeID.push(this.props.quotesToDisplayAddThemeId)
-      //   // } else {
-      //   //   inputValue = []
-      //   // }
-      //   // console.log(this.props.previousAddThemeQuotes)
-      //   // console.log(inputValue)
-
-      //   // else if ((this.props.currentAddThemeQuote !== quote._id) && (this.props.AddThemeId === quote._id)) {
-      //   //   console.log("Helooo asdklfjdslfjd s")
-      //   //   input.value = []
-      //   //   inputValue = input.value
-      //   // }
-      //   // // else if (this.props.AddThemeID !== this.props.currentAddThemeQuote) {
-      //   // //   console.log("INSIDEEEE")
-      //   // //   input.value = []
-      //   // //   inputValue = input.value
-      //   // // }
-      //   // console.log(this.props.AddThemeId)
-      //   // console.log(quote._id)
-      //   // if (this.props.AddThemeId === quote._id) {
-      //   //   console.log(this.props.AddThemeId)
-      //   //   inputValue = input.value
-      //   //   console.log("inputVal", inputValue)
-      //   // } else {
-      //   // inputValue = []
-      //   // }
-        // return (
-        //   { <Multiselect 
-        //     {...input}
-        //     onBlur={() => input.onBlur()}
-        //     value={inputValue}
-        //     disabled={!(this.props.isOpen && this.props.AddThemeId === quote._id)}
-        //     className="selectBox"
-        //     data={data}
-        //     open={this.props.isOpen && this.props.AddThemeId === quote._id}
-        //     defaultValue={[]}
-        //   />
-        // )
-      // }
       addQuoteThemes = themes.filter(theme => {
         return !(quote.theme.includes(theme))
       });
@@ -151,45 +73,11 @@ export class SearchResults extends React.Component {
         <section key={quote._id} className="quote-expanded">
           <h2 className="quoteText"><span>&ldquo;</span>{quote.quoteString}<span>&rdquo;</span></h2>
           <h4 className="searchResultsAuthor"><span>- </span>{quote.author}</h4>
-            <h3>Theme(s): {themesToDisplay}
-            </h3> 
-            {
-            // <div>
-            //   <button onClick={() => this.handleAddThemeClick(quote)} className='addThemeButton'>
-            //       <i className='fa fa-plus fa-fw' aria-hidden='true'></i> {'Theme'}
-            //   </button>
-            // </div>
-            }
-            
+            <h3>Theme(s): {themesToDisplay}</h3> 
             <div className="addThemeAndDeleteBox">
             <AddThemeFormComponent form={`addThemeForm_${quote._id}`} key={quote._id} themeOptions={themeOptions} quote={quote} />
-            {
-             // <form className="addThemeForm" 
-             //       onSubmit={this.props.handleSubmit((value, quote) => 
-             //       this.onSubmit(value,quote))}
-             //       form={`addThemeForm_${quote._id}`}
-             //  >
-             //  <label className="addThemeLabel">Add Theme</label>
-             //  <div>
-            }
-                {
-                // <Field className="individualAddThemeForms" name="themesToAdd" component="select">
-                //   <option>Select Theme</option>
-                //   {themeOptions}
-                // </Field>
-                // <button 
-                //   className="searchSubmitButton"
-                //   type="submit" 
-                //   disabled={this.props.pristine || this.props.submitting}>
-                //   Submit
-                // </button>
-              }
-              </div>
+            </div>
           <div className="deleteBox" >
-            {//<button onClick={() =>
-              //this.handleDeleteClick(quote)}
-              //>
-            }
               <button onClick={(e) => 
                 this.handleDeleteClick(quote, e)} className=" deleteButton btn btn-danger">
                  <i className="fa fa-trash-o fa-lg"></i> Delete</button>
@@ -204,7 +92,6 @@ export class SearchResults extends React.Component {
         </div>
       );
     }
-
     return (
      <div> 
      </div>
