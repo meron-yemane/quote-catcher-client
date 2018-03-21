8e247c54ab9fec3d58aa737dbd5832073b9c1336
meron93 <meronyemane18@gmail.com>
2018-03-20 17:13:43 -0700

    Added react-https-redirect to package.json

--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ import {Provider} from 'react-redux';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import store from './store';
+import HttpsRedirect from 'react-https-redirect';
 
 ReactDOM.render(
   <Provider store={store}>
     <Router>
-      <App />
+      <HttpsRedirect>
+        <App />
+      </HttpsRedirect>
     </Router>
   </Provider>, 
   document.getElementById('root')
